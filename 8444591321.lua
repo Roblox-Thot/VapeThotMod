4601911a25b2f77f9307cdd5cf56fb14dc1ac4e0
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2022-12-10 20:17:44 -0500

    asd

--- 8444591321.lua
+++ 8444591321.lua
@@ -7,32 +7,6 @@ local requestfunc = syn and syn.request or http and http.request or http_request
 local checkpublicreponum = 0
 local checkpublicrepo
 checkpublicrepo = function(id)
-	local suc, req = pcall(function() return requestfunc({
-		Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/CustomModules/".. game.PlaceId ..".lua",
-		Method = "GET"
-	}) end)
-	if not suc then
-		checkpublicreponum = checkpublicreponum + 1
-		spawn(function()
-			local textlabel = Instance.new("TextLabel")
-			textlabel.Size = UDim2.new(1, 0, 0, 36)
-			textlabel.Text = "Loading CustomModule Failed!, Attempts : "..checkpublicreponum
-			textlabel.BackgroundTransparency = 1
-			textlabel.TextStrokeTransparency = 0
-			textlabel.TextSize = 30
-			textlabel.Font = Enum.Font.SourceSans
-			textlabel.TextColor3 = Color3.new(1, 1, 1)
-			textlabel.Position = UDim2.new(0, 0, 0, -36)
-			textlabel.Parent = GuiLibrary["MainGui"]
-			task.wait(2)
-			textlabel:Remove()
-		end)
-		task.wait(2)
-		return checkpublicrepo(id)
-	end
-	if req.StatusCode == 200 then
-		return req.Body
-	end
 	return nil
 end
 local publicrepo = checkpublicrepo(game.PlaceId)
