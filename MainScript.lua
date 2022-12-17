1faf5090af2ad84a7f73df7d5fde9b7e163f6342
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2022-12-17 15:37:58 -0500

    Staying up to date

--- MainScript.lua
+++ MainScript.lua
@@ -21,7 +21,7 @@ local function GetURL(scripturl)
 	end
 end
 local getasset = getsynasset or getcustomasset or function(location) return "rbxasset://"..location end
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
 local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request or function(tab)
 	if tab.Method == "GET" then
 		return {
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-Thot/VapeThotM
 ```
 
 # Additions 
-* Fixed auto launching on game teleport ([Submited a PR](https://github.com/7GrandDadPGN/VapeV4ForRoblox/pull/86))
+* Fixed auto launching on game teleport ([Janky fix on main](https://github.com/7GrandDadPGN/VapeV4ForRoblox/commit/2ff4e7f0b98bec05b7826b4b0c2e7b63f96f16ba#diff-e1efa46e048a1d3fe4fe044ac0c83c73fd209a26f49c9073b10f0b5297cc627bL1679))
 * Added AnimatedCapes
 * Added Force First Person
 
