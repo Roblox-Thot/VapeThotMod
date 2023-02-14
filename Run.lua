b57634b6917f0d66208732d67c551f8b0e6c1ec8
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-02-13 22:17:14 -0500

    make vape folder if it's not a thing

--- Run.lua
+++ Run.lua
@@ -15,29 +15,34 @@ local commit = (getCommit("https://github.com/7GrandDadPGN/VapeV4ForRoblox") or 
 
 local RTcommit = (getCommit("https://github.com/Roblox-Thot/VapeThotMod") or "main")
 
-if ((not isfile("vape/commithash.txt")) or readfile("vape/commithash.txt") ~= commit or (not isfile("vape/RTcommithash.txt")) or readfile("vape/RTcommithash.txt") ~= RTcommit) then
-	for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
-		if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-			delfile(v)
-		end 
-	end
-	if isfolder("vape/CustomModules") then 
-		for i,v in pairs(listfiles("vape/CustomModules")) do 
+if isfolder("vape") then
+	if ((not isfile("vape/commithash.txt")) or readfile("vape/commithash.txt") ~= commit or (not isfile("vape/RTcommithash.txt")) or readfile("vape/RTcommithash.txt") ~= RTcommit) then
+		for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
 			if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
 				delfile(v)
 			end 
 		end
-	end
-	if isfolder("vape/Libraries") then 
-		for i,v in pairs(listfiles("vape/Libraries")) do 
-			if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-				delfile(v)
-			end 
+		if isfolder("vape/CustomModules") then 
+			for i,v in pairs(listfiles("vape/CustomModules")) do 
+				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					delfile(v)
+				end 
+			end
+		end
+		if isfolder("vape/Libraries") then 
+			for i,v in pairs(listfiles("vape/Libraries")) do 
+				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					delfile(v)
+				end 
+			end
 		end
 	end
-	writefile("vape/commithash.txt", commit)
-	writefile("vape/RTcommithash.txt", RTcommit)
+else
+	makefoler("vape")
 end
 
+-- Save hashes
+writefile("vape/commithash.txt", commit)
+writefile("vape/RTcommithash.txt", RTcommit)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-Thot/VapeThotMod/"..RTcommit.."/MainScript.lua", true))()
