ffecb16f966b0fb8c9fc030dd2bd26dd76694f0b
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-02-20 22:17:53 -0500

    Update Run.lua

--- Run.lua
+++ Run.lua
@@ -14,37 +14,32 @@ local commit = (getCommit("https://github.com/7GrandDadPGN/VapeV4ForRoblox") or 
 
 local RTcommit = (getCommit("https://github.com/Roblox-Thot/VapeThotMod") or "main")
 
-
-
 if isfolder("vape") then
-	if ((not isfile("vape/commithash.txt")) or readfile("vape/commithash.txt").."!" ~= commit or (not isfile("vape/RTcommithash.txt")) or readfile("vape/RTcommithash.txt") ~= RTcommit) then
-		for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
+	for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
+		if isfile(v) then
+			delfile(v)
+		end 
+	end
+	if isfolder("vape/CustomModules") then 
+		for i,v in pairs(listfiles("vape/CustomModules")) do 
+			if isfile(v) and readfile(v):find("--this is my anti delete watermark kekw") then
+				delfile(v)
+			end 
+		end
+	end
+	if isfolder("vape/Libraries") then 
+		for i,v in pairs(listfiles("vape/Libraries")) do 
 			if isfile(v) then
 				delfile(v)
 			end 
 		end
-		if isfolder("vape/CustomModules") then 
-			for i,v in pairs(listfiles("vape/CustomModules")) do 
-				if isfile(v) and readfile(v):find("--this is my anti delete watermark kekw") then
-					delfile(v)
-				end 
-			end
-		end
-		if isfolder("vape/Libraries") then 
-			for i,v in pairs(listfiles("vape/Libraries")) do 
-				if isfile(v) then
-					delfile(v)
-				end 
-			end
-		end
-	else
-		-- Save hashes
-		writefile("vape/commithash.txt", commit.."!")
-		
-		writefile("vape/RTcommithash.txt", RTcommit)
 	end
 else
 	makefoler("vape")
 end
 
+-- Save hashesw
+writefile("vape/commithash.txt", commit.."!")
+		
+writefile("vape/RTcommithash.txt", RTcommit)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-Thot/VapeThotMod/"..RTcommit.."/MainScript.lua", true))()
