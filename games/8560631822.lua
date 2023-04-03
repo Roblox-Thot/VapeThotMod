6f9004140edbcf6950d94d111139bb626b4c1821
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-04-02 20:45:45 -0400

    fix

--- games/8560631822.lua
+++ games/8560631822.lua
@@ -4,7 +4,7 @@ local function betterisfile(file)
 end
 
 local function getScript(scripturl)
-	local suc, res = pcall(function() return game:HttpGet"https://raw.githubusercontent.com/Roblox-Thot/VapeThotMod/"..readfile("vape/RTcommithash.txt").."/games/6872274481.lua", true) end)
+	local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/Roblox-Thot/VapeThotMod/"..readfile("vape/RTcommithash.txt").."/games/6872274481.lua", true) end)
 	if not suc or res == "404: Not Found" then return nil end
 	return res
 end
@@ -13,7 +13,7 @@ shared.CustomSaveVape = 6872274481
 if betterisfile("vape/CustomModules/6872274481.lua") then
 	loadstring(readfile("vape/CustomModules/6872274481.lua"))()
 else
-	local publicrepo = vapeGithubRequest("games/6872274481.lua")
+	local publicrepo = getScript("games/6872274481.lua")
 	if publicrepo then
 		loadstring(publicrepo)()
 	end
